f3968a85c55617951167e1374f4cfa2b0c25a33f
Shiva Kanyal <kanyalshiva1@gmail.com>
2020-09-30 13:23:29 +0530

    added text in about section..

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
-
 const messageOne = document.querySelector('.messageOne')
 const messageTwo = document.querySelector('.messageTwo');
 
